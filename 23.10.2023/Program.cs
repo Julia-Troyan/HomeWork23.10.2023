0f03d0a45b8b540ddb8b9c631561dfd261df5ac0
Julia-Tryan <uliana.troyan@mail.ru>
2023-10-23 22:48:20 +0300

    решение первой задачиc с исправлением

--- 23.10.2023/Program.cs
+++ 23.10.2023/Program.cs
@@ -3,25 +3,54 @@
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
 
- using System;
+    Console.Write("Введите число A: ");
+    int A = int.Parse(Console.ReadLine());
 
-class Program
-{
-    static void Main()
+    Console.Write("Введите степень B: ");
+    int B = int.Parse(Console.ReadLine());
+
+    int result = 1;
+
+    for (int i = 0; i < B; i++)
     {
-        Console.Write("Введите число A: ");
-        int A = int.Parse(Console.ReadLine());
-
-        Console.Write("Введите степень B: ");
-        int B = int.Parse(Console.ReadLine());
-
-        int result = 1;
-
-        for (int i = 0; i < B; i++)
-        {
-            result *= A;
-        }
-
-        Console.WriteLine($"{A} в степени {B} равно {result}");
+        result *= A;
     }
-}
+
+    Console.WriteLine($"{A} в степени {B} равно {result}");
+
+
+
+
+
+// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+
+// Console.Write("Введите число: ");
+// string input = Console.ReadLine();
+
+// if (int.TryParse(input, out int number))
+// {
+//     int sumOfDigits = CalculateSumOfDigits(number);
+//     Console.WriteLine($"Сумма цифр числа {number} равна {sumOfDigits}");
+// }
+// else
+// {
+//     Console.WriteLine("Неверный формат числа. Пожалуйста, введите целое число.");
+// }
+
+
+// int CalculateSumOfDigits(int num)
+// {
+//     int sum = 0;
+
+//     while (num != 0)
+//     {
+//         sum += num % 10; // Получаем последнюю цифру числа и добавляем её к сумме.
+//         num /= 10;       // Убираем последнюю цифру числа.
+//     }
+
+//     return sum;
+// }
